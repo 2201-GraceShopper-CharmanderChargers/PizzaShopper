910e486dac2a63f8e3d5c1127053b982a12109b3
ShreddedPork <r.ho@miami.edu>
2022-03-01 12:48:23 -0500

    added pizza getting route

---
+++ server/api/pizzas.js
@@ -0,0 +1,12 @@
+const Router = require('express').Router();
+const Pizza = require('../db/models/pizza')
+const Order = require('../db/models/order')
+
+Router.get('/', async (req, res, next) => {
+  try {
+    const allPizzas = await Pizza.findAll()
+    res.send(allPizzas)
+  } catch (error) {
+    next(error)
+  }
+});

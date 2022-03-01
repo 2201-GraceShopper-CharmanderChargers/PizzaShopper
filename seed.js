0371c2411e69b43e3ebd96d5f2913f80d64c2134
David Dunham <dd567@cornell.edu>
2022-03-01 11:57:06 -0500

    created dummy data

--- seed.js
+++ seed.js
@@ -1,11 +1,63 @@
 const { green, red } = require('chalk');
 const { db, Pizza, User, Order } = require('./server/db');
 
+const fakePizzas = [
+  {
+    name: 'margharita',
+    description: 'tomato and mozzarella!',
+    price: 7.99,
+    imageUrl:
+      'https://cdn.loveandlemons.com/wp-content/uploads/2019/09/margherita-pizza-500x500.jpg',
+  },
+  {
+    name: 'capricciosa',
+    description: 'ham, cheese, and mushrooms',
+    price: 12.99,
+    imageUrl:
+      'https://cdn.tasteatlas.com/images/dishes/6b28b438a3b64a7e9fca9081c89f07ff.jpg',
+  },
+  {
+    name: 'three cheese',
+    description: 'uh...chese? This is per slice by the way',
+    price: 17.99,
+    imageUrl:
+      'https://veenaazmanov.com/wp-content/uploads/2020/06/3-cheese-pizza-with-no-knead-pizza-dough3.jpg',
+  },
+];
+
+const fakeUsers = [
+  {
+    firstName: 'David',
+    lastName: 'Dunham',
+    password: '12345',
+    email: 'daviddfh@gmail.com',
+  },
+  {
+    firstName: 'Sam',
+    lastName: 'Smith',
+    password: 'password',
+    email: 'daviddfh@gmail.com',
+  },
+  {
+    firstName: 'Joe',
+    lastName: 'Blow',
+    password: 'password',
+    email: 'daviddfh@gmail.com',
+  },
+];
+
+const fakeOrders = [
+  { address: '123 Deerborn Lane' },
+  { address: '234 Glen Oaks Court' },
+  { address: '6123 Deerpath Road' },
+];
+
 const seed = async () => {
   try {
     await db.sync({ force: true });
-
-    // seed your database here!
+    await Promise.all(fakePizzas.map((pizza) => Pizza.create(pizza)));
+    await Promise.all(fakeOrders.map((order) => Order.create(order)));
+    await Promise.all(fakeUsers.map((user) => User.create(user)));
   } catch (err) {
     console.log(red(err));
   }

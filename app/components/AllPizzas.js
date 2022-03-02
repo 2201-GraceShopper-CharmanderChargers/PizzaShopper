307263c32944e82e9195773e90eab7568a2ce113
David Dunham <dd567@cornell.edu>
2022-03-02 15:58:32 -0500

    unbroke after our failed merge

--- app/components/AllPizzas.js
+++ app/components/AllPizzas.js
@@ -13,7 +13,6 @@ export class AllPizzas extends React.Component {
     super(props);
 
     this.loading = true;
-  
   }
 
   componentDidMount() {
@@ -24,7 +23,6 @@ export class AllPizzas extends React.Component {
     if (prevprops.pizzas !== this.props.pizzas) {
       console.log('we updated the component');
     }
-   
   }
   render() {
     const pizzas = this.props.pizzas;
